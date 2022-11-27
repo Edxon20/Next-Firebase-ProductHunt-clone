4de0436237f366f2391120e527947358f5f0d301
Edxon20 <andrade20eduardo@gmail.com>
2022-11-27 11:11:37 -0400

    [ADD] dependences @emotion

--- produnthuntnext/pages/index.tsx
+++ produnthuntnext/pages/index.tsx
@@ -6,6 +6,15 @@ export default function Home() {
   return (
     <div>
       <h1>Inicio</h1>
+
+      <style jsx>{`
+          h1{
+            color:red;
+          }
+
+        `}
+
+      </style>
     </div>
   )
 }
