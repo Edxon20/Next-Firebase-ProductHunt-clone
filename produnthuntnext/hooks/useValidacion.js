ff24af07c9de37f4c035df777390f011af7363c7
John Doe <edixon20pes@gmail.com>
2022-11-29 11:18:30 -0400

    [ADD] hooks validation

--- produnthuntnext/hooks/useValidacion.js
+++ produnthuntnext/hooks/useValidacion.js
@@ -1,10 +1,24 @@
-import React, {useState} from 'react';
-
-
+import React, {useState, useEffect} from 'react';
 
 const useValidacion = (stateInicial, validar, fn) => {
 
     const [valores, guardarValores ] = useState(stateInicial);
+    const [errores, guardarErrores] = useState({});
+    const [submitForm, guardarSubmitForm] = useState(false);
+
+    useEffect(() =>{
+
+        if(submitForm){
+            const noErrores = Object.keys(errores).length === 0;
+
+            if(noErrores){
+                fn(); // Funcion que se ejecuta en el componente. 
+            }
+        }
+
+
+    },[] )
+
 
     return(
         <h1>hola</h1>
