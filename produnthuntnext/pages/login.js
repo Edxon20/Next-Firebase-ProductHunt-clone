c7c3f0b5f91755e07ac759818909ecea3beffb10
John Doe <edixon20pes@gmail.com>
2022-11-30 10:59:45 -0400

    [Create] Login

---
+++ produnthuntnext/pages/login.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react'
+import Router from 'next/router';
+import Layout from '../components/layout/Layout'
+import { Error, Campo, Form, InputSubmit } from '../components/ui/Formulario'
+import { css } from "@emotion/react"
+
+import firebase from '../firebase'
+//validaciones
+import useValidacion from '../hooks/useValidacion'
+import validarIniciarSesion from '../validacion/validarIniciarSesion'
+
+const STATE_INITIAL ={
+  email: '',
+  password: ''
+}
+
+
+const login = () => {
+
+  const [error, guardarError] = useState(false);
+
+  const {valores,errores,handleSubmit,handleChange,handleBlur} = useValidacion(STATE_INITIAL,validarIniciarSesion, iniciarSesion);
+
+  const {email, password} = valores;
+
+   function iniciarSesion () {
+      console.log('Iniciar Sesion...')
+  }
+  return (
+    <div>
+      <Layout>
+        <>
+          <h1
+            css={css`
+                text-align: center;
+                margin-top: 5rem;
+            `}
+          >Iniciar Sesion</h1>
+          <Form
+            onSubmit={handleSubmit}
+            noValidate
+          >
+       
+            <Campo>
+              <label htmlFor='email' >Email</label>
+              <input
+                type='text'
+                id='email'
+                placeholder='Tu email'
+                name='email'
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+            { errores.email && <Error>{errores.email}</Error>  }
+            <Campo>
+              <label htmlFor='password' >Email</label>
+              <input
+                type='password'
+                id='password'
+                placeholder='Tu password'
+                name='password'
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+            { errores.password && <Error>{errores.password}</Error>  }
+
+            <InputSubmit
+              type='submit'
+              value='Iniciar Sesion'
+            />
+          </Form>
+        </>
+      </Layout>
+    </div>
+  )
+}
+
+export default login;
